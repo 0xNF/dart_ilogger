ba3a97bebff3e79dcbc0a4e872ce420f2cba9186
0xnf <0xnf@wintech.com>
2023-09-12 16:12:26 +0900

    added impossible simple console basic logger

--- lib/src/ilogger.dart
+++ lib/src/ilogger.dart
@@ -81,3 +81,64 @@ abstract class ILogger implements ISuppress {
     return null;
   }
 }
+
+final class BasicConsoleLogger extends ILogger {
+  BasicConsoleLogger({String? name}) : _name = name ?? 'BasicLogger';
+
+  @override
+  String get name => _name;
+  final String _name;
+
+  @override
+  void debug(message, {Exception? exception, Map<String, dynamic>? eventProperties}) {
+    log(message, level: LogLevel.debug, exception: exception, eventProperties: eventProperties);
+  }
+
+  @override
+  void error(message, {Exception? exception, Map<String, dynamic>? eventProperties}) {
+    log(message, level: LogLevel.error, exception: exception, eventProperties: eventProperties);
+  }
+
+  @override
+  void fatal(message, {Exception? exception, Map<String, dynamic>? eventProperties}) {
+    log(message, level: LogLevel.fatal, exception: exception, eventProperties: eventProperties);
+  }
+
+  @override
+  void info(message, {Exception? exception, Map<String, dynamic>? eventProperties}) {
+    log(message, level: LogLevel.info, exception: exception, eventProperties: eventProperties);
+  }
+
+  @override
+  bool get isDebugEnabled => true;
+
+  @override
+  bool isEnabled(LogLevel level) => true;
+
+  @override
+  bool get isErrorEnabled => true;
+
+  @override
+  bool get isFatalEnabled => true;
+  @override
+  bool get isInfoEnabled => true;
+  @override
+  bool get isTraceEnabled => true;
+  @override
+  bool get isWarnEnabled => true;
+
+  @override
+  void log(message, {required LogLevel level, Exception? exception, Map<String, dynamic>? eventProperties}) {
+    print('${DateTime.now()} [$level] $message |$exception|$eventProperties');
+  }
+
+  @override
+  void trace(message, {Exception? exception, Map<String, dynamic>? eventProperties}) {
+    log(message, level: LogLevel.trace, exception: exception, eventProperties: eventProperties);
+  }
+
+  @override
+  void warn(message, {Exception? exception, Map<String, dynamic>? eventProperties}) {
+    log(message, level: LogLevel.warn, exception: exception, eventProperties: eventProperties);
+  }
+}
